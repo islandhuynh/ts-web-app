78e82a07b3d54621a7d65894bb72411edc7029d2
unknown <islandhuynh@gmail.com>
2021-02-23 14:02:26 -0500

    initialized user form

--- src/models/Model.ts
+++ src/models/Model.ts
@@ -26,9 +26,9 @@ export class Model<T extends HasId> {
         private events: Events,
         private sync: Sync<T>
     ){}
-
+    
     on = this.events.on;
-    trigger = this.events.on;
+    trigger = this.events.trigger;
     get = this.attributes.get;
 
     set(update: T): void {
