40c57cbd83cda860a6fff274a02641300505e84a
unknown <islandhuynh@gmail.com>
2021-02-23 01:05:23 -0500

    Added accessors in User class to call on functions of 'sub-classes'

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import { User } from './models/User';
 
-const user = new User({ id: 1 });
+const user = new User({ name: 'new record', age: 0 });
 
-user.fetch();
+// A quick reminder on accessors
+// class Person {
+//     constructor(public firstName: string, public lastName: string) {}
 
-setTimeout(() => {
-    console.log(user);
-}, 4000);
+//     // getter allows you to not have to call a function
+//     get fullName(): string {
+//         return `${this.firstName} ${this.lastName}`;
+//     }
+// }
+
+// const person = new Person('firstname', 'lastname');
+// console.log(person.fullName);
+
+user.on('change', () => {
+    console.log('user was changed');
+});
---
+++ src/models/Sync.ts
@@ -0,0 +1,25 @@
+import axios, { AxiosPromise } from 'axios';
+
+interface HasId {
+    id?: number; 
+}
+
+export class Sync<T extends HasId> {
+    constructor(public rootUrl: string) {}
+
+    fetch(id: number): AxiosPromise {
+        return axios.get(`${this.rootUrl}/${id}`);
+    }
+
+    save(data: T): AxiosPromise {
+        const { id } = data;
+
+        // number 0 is falsy value in JS, so we use if(id !== undefined) instead of if(id)
+        if (id !== undefined ) {
+            return axios.put(`${this.rootUrl}/${id}`, data);
+        } else {
+            return axios.post(this.rootUrl, data);
+        }
+    }
+}
+
